34f16cf3e79b3cbe12ff755b8b5385a034785127
Thin Creator 3483 <102002463+SaxbyMod@users.noreply.github.com>
2023-12-13 20:36:25 -0500

    Add Configs to Config.cs
    HardLimit: Sets whether theres a hard llimit
    LimitMode: Sets Limit Mode
    RegionsBeforeFinalBoss: Int to determine how many regions before Leshy/Royal/Etc. Boss
    RepeatBoss: Boolean to determine whether previous bosses can be looped again 
    RepeatRegion: Boolean to determine whether previous regions can be looped again.
    RegionHardLimit: Int to define how many Regions Total per run regardless of how many times youve beat leshy
    HowManyBossesTillRunEnd: Int to determine how Many Major Boss battles you  have to beat before run end

--- Configs.cs
+++ Configs.cs
@@ -4,20 +4,122 @@ namespace EndlessMode;
 
 public static class Configs
 {
- //    public static bool VerticalItems
- //    {
- //        get => m_verticalItems.Value;
- //        set
- //        {
- //            m_verticalItems.Value = value;
- //            Plugin.Instance.Config.Save();
- //        }
- //    }
- //
- //    private static ConfigEntry<bool> m_verticalItems = Bind("General", "Display Items Vertically", false, "Should the items be displayed on vertically?");
- //
- //    private static ConfigEntry<T> Bind<T>(string section, string key, T defaultValue, string description)
-	// {
-	// 	return Plugin.Instance.Config.Bind(section, key, defaultValue, new ConfigDescription(description, null, Array.Empty<object>()));
-	// }
+	public static bool HardLimit
+	{
+		get => m_HardLimit?.Value;
+		set
+		{
+			m_HardLimit.Value = value;
+			Plugin.Instance.Config.Save();
+		}
+	}
+ 
+	private static ConfigEntry<bool> m_HardLimit = Bind("General", "Hard Limit?", false, "Should there be a Hard Limit on the run?");
+	
+	private static ConfigEntry<T> Bind<T>(string section, string key, T defaultValue, string description)
+	{
+		return Plugin.Instance.Config.Bind(section, key, defaultValue, new ConfigDescription(description, null, Array.Empty<object>()));
+	}
+	
+	public static string LimitMode
+	{
+		get => m_LimitMode.Value;
+		set
+		{
+			m_LimitMode.Value = value;
+			Plugin.Instance.Config.Save();
+		}
+	}
+ 
+	private static ConfigEntry<string> m_LimitMode = Bind("General", "Limit Mode", "", "What Type of Limit should be on if HardLimit is enabled? [RegionHardLimit] [FinalBossHardLimit]");
+	
+	private static ConfigEntry<T> Bind<T>(string section, string key, T defaultValue, string description)
+	{
+		return Plugin.Instance.Config.Bind(section, key, defaultValue, new ConfigDescription(description, null, Array.Empty<object>()));
+	}
+
+	public static integer RegionsBeforeFinalBoss
+	{
+		get => m_RegionsBeforeFinalBoss.Value;
+		set
+		{
+			m_RegionsBeforeFinalBoss.Value = value;
+			Plugin.Instance.Config.Save();
+		}
+	}
+ 
+	private static ConfigEntry<integer> m_RegionsBeforeFinalBoss = Bind("General", "Regions Before Final Boss", 3, "How many regions should you go through before a Final Boss?");
+	
+	private static ConfigEntry<T> Bind<T>(string section, string key, T defaultValue, string description)
+	{
+		return Plugin.Instance.Config.Bind(section, key, defaultValue, new ConfigDescription(description, null, Array.Empty<object>()));
+	}
+	
+	public static boolean RepeatBoss
+	{
+		get => m_RepeatBoss.Value;
+		set
+		{
+			m_RepeatBoss.Value = value;
+			Plugin.Instance.Config.Save();
+		}
+	}
+ 
+	private static ConfigEntry<boolean> m_RepeatBoss = Bind("General", "Repeat Boss?", false, "Should bosses have the ability to be Repeated? this means it can be Prospecter into Prospecter. (With different Regions utilized if RepeatRegions is disabled.) [Prospecter, Angler, Trapper, etc.]");
+	
+	private static ConfigEntry<T> Bind<T>(string section, string key, T defaultValue, string description)
+	{
+		return Plugin.Instance.Config.Bind(section, key, defaultValue, new ConfigDescription(description, null, Array.Empty<object>()));
+	}
+	
+	public static boolean RepeatRegion
+	{
+		get => m_RepeatRegion.Value;
+		set
+		{
+			m_RepeatRegion.Value = value;
+			Plugin.Instance.Config.Save();
+		}
+	}
+ 
+	private static ConfigEntry<boolean> m_RepeatRegion = Bind("General", "Repeat Region?", false, "Should Regions be Repeated? this means it can be Woodlands into Woodlands. (With different Bosses at the end if RepeatBosses is disabled.) [Woodlands, Wetlands, Alpine, etc.]");
+	
+	private static ConfigEntry<T> Bind<T>(string section, string key, T defaultValue, string description)
+	{
+		return Plugin.Instance.Config.Bind(section, key, defaultValue, new ConfigDescription(description, null, Array.Empty<object>()));
+	}
+
+	public static integer RegionHardLimit
+	{
+		get => m_RegionHardLimit.Value;
+		set
+		{
+			m_RegionHardLimit.Value = value;
+			Plugin.Instance.Config.Save();
+		}
+	}
+ 
+	private static ConfigEntry<integer> m_RegionHardLimit = Bind("HardLimit", "Region Hard Limit", 9999999999999999, "If LimitMode is set to [RegionHardLimit] how many regions should be the limit?");
+	
+	private static ConfigEntry<T> Bind<T>(string section, string key, T defaultValue, string description)
+	{
+		return Plugin.Instance.Config.Bind(section, key, defaultValue, new ConfigDescription(description, null, Array.Empty<object>()));
+	}
+
+	public static integer FinalBossHardLimit
+	{
+		get => m_FinalBossHardLimit.Value;
+		set
+		{
+			m_FinalBossHardLimit.Value = value;
+			Plugin.Instance.Config.Save();
+		}
+	}
+ 
+	private static ConfigEntry<integer> m_FinalBossHardLimit = Bind("HardLimit", "Final Boss Hard Limit", 9999999999999999, "If LimitMode is set to [FinalBossHardLimit] how many FinalBosses should be the limit?");
+	
+	private static ConfigEntry<T> Bind<T>(string section, string key, T defaultValue, string description)
+	{
+		return Plugin.Instance.Config.Bind(section, key, defaultValue, new ConfigDescription(description, null, Array.Empty<object>()));
+	}
 }
